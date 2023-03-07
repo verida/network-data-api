6ee7df9e2e75c88ea9b33d36b1fe646df48624d9
Chris <chris@verida.io>
2023-03-07 12:04:38 +1030

    Use new helper methods in protocol

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,11 @@ import Controller from './controller'
 
 const router = express.Router()
 
+// /<did>/<contextName>/<databaseName>/<recordId>/<attribute>/<...deepAttributes>
+router.get(/(did\:.*)$/, Controller.getData)
 
-router.get(/(did\:[^\/]*)\/([^\/]*)\/([^\/]*)\/(.*)/, Controller.getData)
-
-
-// router.get('/record/:id', Controller.getRow)
-// router.get('/value/:id', Controller.getValue)
-// router.get('/db/:databaseName', Controller.getDb)
+// /<base58EncodedVeridaUri>
+// @see @verida/helpers Utils.encodeUri()
+router.get(/^\/(.*)$/, Controller.getUri)
 
 export default router
